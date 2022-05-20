8c4dce655cb7174e853bbe5fbcf66f7ac6576a4b
andyTsai2321 <s106667@gmail.com>
2022-05-20 20:26:36 +0800

    no message

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,11 @@
 const handleError = require('../service/handleError');
 const handleSuccess = require('../service/handleSuccess');
-const Posts = require('../model/user');
+const User = require('../model/user');
 
 const user = {
   async getUsers(req,res){
     const users = await User.find();
-    handleSuccess(res, getPosts);
+    handleSuccess(res, users);
   }
 }
 module.exports = user;
